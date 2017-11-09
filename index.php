0d09d0b8fcd3a0af6f09042be5f2018a28a7aa47
EnrikasNemc <33262515+EnrikasNemc@users.noreply.github.com>
2017-11-09 19:25:25 +0200

    styled table

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Backery</title>
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+</head>
+<body>
+
+<table id="border1">
+	<thead id="backgrounds">
+		<tr>
+			<td rowspan="2">Kepinys</td>
+			<td colspan="5">2017-11-09</td>
+		</tr>
+		<tr>
+			<td>VL</td><!--Vakarykštis likutis -->
+			<td>PG</td><!--Pagaminta -->
+			<td>PR</td><!--Parduota -->
+			<td>SG</td><!--Sugadinta -->
+			<td>GL</td><!--Galutinis -->
+		</tr>
+	</thead>
+	<tbody id="border1">
+		
+		<tr>
+			<td>Pyragas</td>
+		</tr>
+		<tr>
+			<td>Bandelė</td>
+		</tr>
+		<tr>
+			<td>Bulkutė</td>
+		</tr>
+		<tr>
+			<td>Sausainis</td>
+		</tr>
+		<tr>
+			<td>Tortas</td>
+		</tr>
+	</tbody>
+</table>
+
+</body>
+</html>
