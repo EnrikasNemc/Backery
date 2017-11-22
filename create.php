835eea225eeb154426f6f6869522aeda3d2fccd2
EnrikasNemc <33262515+EnrikasNemc@users.noreply.github.com>
2017-11-22 17:45:16 +0200

    added if but default is 0

--- create.php
+++ create.php
@@ -2,14 +2,31 @@
 $new_data = $_POST;
 
 //TODO:
-//CHeck if all parameters are rovided
+//Check if all parameters are provided
 
+function checkIfSet(&$existing_data, $new_data){
+	if (isset($new_data["vl"])==true) {
+		if (isset($new_data["pg"])==true) {
+			if (isset($new_data["pr"])==true) {
+				if (isset($new_data["sg"])==true) {
+					if (isset($new_data["gl"])==true) {
+						updateData($existing_data, $new_data);
 
+					}
+				}
+			}
+		}
+	}
+	else{
+		echo "one or more is not set";
+	}
+}	
 
 //reading data
 $existing_data = json_decode (file_get_contents("app/data/bakery-data.json"));
 $existing_data = objectToArray($existing_data);
-updateData($existing_data, $new_data);
+
+checkIfSet($existing_data,$new_data);
 
 print_r(json_encode($existing_data));
 
@@ -30,32 +47,34 @@ function objectToArray(stdClass $obj) : array
 }
 
 
+
 function updateData(&$existing_data, $new_data){
 	if(isset($existing_data[$new_data['date']]))
-	{
-		if (isset($existing_data[$new_data['date']][$new_data['product']])) {
-			echo "Error";
-		}
-		else{
-			$existing_data = createProduct($existing_data, $new_data);
-		}
+{
+	if (isset($existing_data[$new_data['date']][$new_data['product']])) {
+		echo "Error";
 	}
+	else{
+		$existing_data = createProduct($existing_data, $new_data);
+	}
+}
 	else{
 		$existing_data[$new_data['date']] = [];
 
 		$existing_data = createProduct($existing_data, $new_data);
-		
+								
 	}
 }
+
 function createProduct($existing_data, $new_data){
 
-		$existing_data[$new_data['date']][$new_data['product']] = 
-		[
-			(int) $new_data["vl"],
-			(int) $new_data["pg"],
-			(int) $new_data["pr"],
-			(int) $new_data["sg"],
-			(int) $new_data["gl"]
-		];
-		return $existing_data;
+	$existing_data[$new_data['date']][$new_data['product']] = 
+	[
+		(int) $new_data["vl"],
+		(int) $new_data["pg"],
+		(int) $new_data["pr"],
+		(int) $new_data["sg"],
+		(int) $new_data["gl"]
+	];
+	return $existing_data;
 }
